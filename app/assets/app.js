1b9a95208e4767cac6abcc8b60c7a1fa691b8fe8
MathisJANKOVIC <mathis.jankovic@gmail.com>
2023-11-23 01:55:10 +0100

    init symfony

--- app/assets/app.js
+++
@@ -1,7 +0,0 @@
-import { registerReactControllerComponents } from '@symfony/ux-react';
-import './bootstrap.js';
-import './styles/app.scss';
-
-require('bootstrap');
-
-registerReactControllerComponents(require.context('./react/apps', true, /\.(j|t)sx?$/));
